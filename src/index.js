6f972d9c4342aebe23ed36b1a825863ae8157075
vlad <longsno@gmail.com>
2021-02-16 19:28:15 +0300

    tests passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  console.log(str);
+  let i = 0,
+      b  = [];
+
+  bracketsConfig.forEach(element => {
+    b.push(element.join(''));
+  });
+
+  while (i < b.length) {
+    if(str.includes(b[i])){
+      str = str.replace(b[i], '');
+      i = 0;
+    } else i++;
+  }
+
+  if (str.length === 0) return true;
+  else return false;
+
 }
